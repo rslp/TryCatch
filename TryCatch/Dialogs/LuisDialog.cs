a4808b13f602f65e82ecbc719ae1c2decae3ea4c
rslp <liushuai_use@163.com>
2017-07-01 22:39:09 +0800

    新增Luis后台处理--刘帅

--- TryCatch/Dialogs/LuisDialog.cs
+++ TryCatch/Dialogs/LuisDialog.cs
@@ -13,39 +13,131 @@ namespace TryCatch.Dialogs
     [Serializable]
     public class LuisDialog : LuisDialog<object>
     {
-        /*
-        [LuisIntent("打招呼")]
-        public async Task Greeting(IDialogContext context, LuisResult result)
+        
+        [LuisIntent("查价值和意义类")]
+        public async Task Find_Value(IDialogContext context, LuisResult result)
         {
-            await context.PostAsync("你好啊，老铁");
-            context.Wait(MessageReceived);
-        }*/
-        /*
-        [LuisIntent("询问名字")]
-        public async Task Name(IDialogContext context, LuisResult result)
-        {
-            await context.PostAsync("你就叫我特浪扑吧。");
+            //获取entity列表
+            IList<EntityRecommendation> entity1 = result.Entities;
+
+            //遍历entity列表
+            foreach (EntityRecommendation example in entity1)
+            {
+                //获取entity
+                string entity = example.Entity;
+                //获取entity的name
+                string type = example.Type;
+                //判断entity类别
+                if(type != null)
+                {
+                    if (type.Equals("学院"))
+                    {
+                        switch (entity)
+                        {
+                            case "继续教育学院":
+                                //回复消息
+                                await context.PostAsync("信息时代终身学习的推动者！");
+                                break;
+                            default:
+                                string question = result.Query;
+                                string solution = await QnAMaker.MakeRequest(question);
+
+                                if (solution == null)
+                                {
+                                    await context.PostAsync("很抱歉 没能帮您找到答案");
+                                }
+                                else await context.PostAsync(solution);
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        string question = result.Query;
+                        string solution = await QnAMaker.MakeRequest(question);
+
+                        if (solution == null)
+                        {
+                            await context.PostAsync("很抱歉 没能帮您找到答案");
+                        }
+                        else await context.PostAsync(solution);
+                        break;
+                    }
+                }              
+            }
             context.Wait(MessageReceived);
         }
-        [LuisIntent("讲笑话")]
-        public async Task Amuse(IDialogContext context, LuisResult result)
+
+        [LuisIntent("查功能")]
+        public async Task Find_Function(IDialogContext context, LuisResult result)
         {
-            await context.PostAsync("你的存在其实就是一个笑话。");
+            //获取entity列表
+            IList<EntityRecommendation> entity1 = result.Entities;
+
+            //遍历entity列表
+            foreach (EntityRecommendation example in entity1)
+            {
+                //获取entity
+                string entity = example.Entity;
+                //获取entity的name
+                string type = example.Type;
+                //判断entity类别
+                if (type != null)
+                {
+                    if (type.Equals("学院"))
+                    {
+                        switch (entity)
+                        {
+                            case "创新创业学院":
+                                //回复消息
+                                await context.PostAsync("（1）人才培养（2）产业引导（3）助创空间");
+                                break;
+                            case "感知技术研究院学院":
+                                //回复消息
+                                await context.PostAsync("技术创新，人才培养，公共服务，成果转化");
+                                break;
+                            case "信息光子学与光通信研究院":
+                                //回复消息
+                                await context.PostAsync("其主要职能是支撑信息光子学与光通信国家重点实验室的建设和发展");
+                                break;
+                            case "中共北京邮电大学网络技术研究院党委":
+                                //回复消息
+                                await context.PostAsync("根据《中国共产党党章》的有关规定，院党委对学院党建设工作负全面领导责任，党委书记为第一责任人，其他成员根据工作分工，对职责范围内的工作负直接领导责任。");
+                                break;
+                            case "理学院":
+                                //回复消息
+                                await context.PostAsync("学院办公室本着“高效服务、务实协调”的宗旨，承担日常党政、教学、科研管理、学生管理和服务的大量工作。目前下设两个综合办公室，现有工作人员6名，其中副高级以上职称3名；中级职称3名。");
+                                break;
+                            case "人文学院":
+                                //回复消息
+                                await context.PostAsync("中心承担全校9个学院，2个研究院本科及研究生公共基础实验教学工作，现为全校36个本科专业41个研究生专业开设60门实验课程。");
+                                break;
+                            default:
+                                string question = result.Query;
+                                string solution = await QnAMaker.MakeRequest(question);
+
+                                if (solution == null)
+                                {
+                                    await context.PostAsync("很抱歉 没能帮您找到答案");
+                                }
+                                else await context.PostAsync(solution);
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        string question = result.Query;
+                        string solution = await QnAMaker.MakeRequest(question);
+
+                        if (solution == null)
+                        {
+                            await context.PostAsync("很抱歉 没能帮您找到答案");
+                        }
+                        else await context.PostAsync(solution);
+                    }
+                }
+            }
             context.Wait(MessageReceived);
         }
-        [LuisIntent("查询学校")]
-        public async Task FindSchool(IDialogContext context, LuisResult result)
-        {
-            await context.PostAsync("北邮是个好地方。");
-            context.Wait(MessageReceived);
-        }
-        [LuisIntent("")]
-        public async Task None(IDialogContext context, LuisResult result)
-        {
-            await context.PostAsync("老铁，发烧就该吃药，不能胡言乱语啊。");
-            context.Wait(MessageReceived);
-        }
-        */
         [LuisIntent("None")]
         public async Task None(IDialogContext context, LuisResult result)
         {
